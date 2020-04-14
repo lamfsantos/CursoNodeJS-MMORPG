27c02b27e148d885a42e0d8c590a15d28976150f
Furlan <lamfsantos5@gmail.com>
2020-04-13 22:33:20 -0400

    Recebendo e validadndo dados da view de cadastro

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,20 @@
 module.exports.cadastro = function(application, request, response) {
-	response.render('cadastro')
+	response.render('cadastro', {validacao: {}, dadosForm: {}})
+}
+
+module.exports.cadastrar = function(application, request, response) {
+	var dadosForm = request.body
+	request.assert('nome', 'Nome não pode ser vazio').notEmpty()
+	request.assert('usuario', 'Usuário não pode ser vazio').notEmpty()
+	request.assert('senha', 'Senha não pode ser vazio').notEmpty()
+	request.assert('casa', 'Casa não pode ser vazio').notEmpty()
+
+	var errors = request.validationErrors()
+
+	if (errors) {
+		response.render('cadastro', {validacao: errors, dadosForm: dadosForm})
+		return
+	}
+
+	response.send("Podemos cadastrar")
 }
