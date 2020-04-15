9f1330f6477625ff75531fa7c080ec16a3cf7905
Furlan <lamfsantos5@gmail.com>
2020-04-15 16:27:20 -0400

    Instalação do express-session e configuração do controller de autenticação

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,19 @@
 module.exports.index = function(application, request, response) {
-	response.render('index')
+	response.render('index', {validacao: {}})
+}
+
+module.exports.autenticar = function(application, request, response) {
+	var dadosForm = request.body
+
+	request.assert('usuario', 'Usuario não pode ser vazio').notEmpty()
+	request.assert('senha', 'Senha não pode ser vazio').notEmpty()
+
+	var errors = request.validationErrors()
+
+	if (errors) {
+		response.render("index", {validacao: errors})
+		return
+	}
+
+	response.send('Tudo ok para criar a sessão')
 }
