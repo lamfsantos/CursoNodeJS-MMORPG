aca8a1b601804740c786ad65359e57a803b230b7
Luiz André Furlan <lamfsantos5@gmail.com>
2020-04-22 20:54:20 -0400

    Criando cronometro pro tempo das ações

--- app/models/JogoDAO.js
+++ app/models/JogoDAO.js
@@ -62,7 +62,9 @@ JogoDAO.prototype.acao = function(acao) {
 JogoDAO.prototype.getAcoes = function(usuario, response) {
 	this._connection.open(function(error, mongoclient){
 		mongoclient.collection("acao", function(error, collection){
-			collection.find({usuario: usuario}).toArray(function(error, result){
+			var date = new Date()
+			var momento_atual = date.getTime()
+			collection.find({usuario: usuario, acao_termina_em: {$gt: momento_atual}}).toArray(function(error, result){
 				response.render("pergaminhos", {acoes: result})
 			})
 			mongoclient.close()
